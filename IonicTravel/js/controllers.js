94c723be6d2243c9cb1a6850c23a1e5cf9cb7550
sim.romano@gmail.com <sim.romano@gmail.com>
2014-05-28 18:20:41 +0200

    log contact

--- IonicTravel/js/controllers.js
+++ IonicTravel/js/controllers.js
@@ -97,6 +97,7 @@ function (Clients, $state, $scope, $ionicModal) {
 	$scope.loadFromContacts = function () {
 		// badrit
 		window.plugins.ContactPicker.chooseContact(function (contactInfo) {
+			console.log(contactInfo.displayName);
 			$scope.form.description.$setViewValue(contactInfo.displayName);
 		});
 
